c4520c4e14aa70b8852e02b999db836a8b8619ed
Caitlin Ridlen <caitlin@ridlen.net>
2011-03-24 07:56:25 -0500

    add calc.rb

---
+++ calc.rb
@@ -0,0 +1,22 @@
+puts 1+2
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+puts 7 + 8 * (5 - 2) 
+puts 5 + 2 - 6 + 4 / (4 + 16 - 8)
+puts 24 * 365
+puts 10 * 365 * 24 * 60 
+puts 24 * 365 * 24 * 60 * 60
+puts 1025000000 / 60 / 60 / 24/ 365 
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
