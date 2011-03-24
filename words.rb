b15a73eccb9cbb88336d91741df021f4d804f934
Caitlin Ridlen <caitlin@ridlen.net>
2011-03-24 07:57:09 -0500

    add words.rb

---
+++ words.rb
@@ -0,0 +1,10 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts 'I like' + 'apple pie'
+puts 'I like ' + 'apple pie'
+puts 'blink ' * 4
+puts 'You\'re swell!'
+puts ' i love\ phil'
+puts ' i love\\ phil'
+puts 
